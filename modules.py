a2547aca22e527d1d7824521018f8064a4942150
Youngjae Yu <mycalljordan@snu.ac.kr>
2019-03-15 21:35:00 +0900

    .

--- modules.py
+++ modules.py
@@ -256,3 +256,50 @@ class TCBAM(nn.Module):
             x_out = self.SpatialGate(x_out)
         x_out = self.TemporalGate(x_out)
         return x_out
+    
+    
+class ConcatShiftGate(nn.Module):
+    def __init__(self, num_segments, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+        super(ConcatShiftGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+            )
+        self.pool_types = pool_types
+    def forward(self, x):
+        channel_att_sum = None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( avg_pool )
+            elif pool_type=='max':
+                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( max_pool )
+            elif pool_type=='lp':
+                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( lp_pool )
+            elif pool_type=='lse':
+                # LSE pool only
+                lse_pool = logsumexp_2d(x)
+                channel_att_raw = self.mlp( lse_pool )
+
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = torch.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        
+        
+        size = x.size()
+        # (N*T, C, H, W)
+        x = x.view((-1, self.num_segments) + size[1:])
+        # (N, T, C, H, W)
+        x_shift = torch.zeros_like(x)
+        x_shift[:, 1:, ...] = x[:, :-1, ...]
+        
+        
+        return x * scale + x_shift * (1.0-scale)
